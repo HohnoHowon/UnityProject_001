d057656325d26c6fdf356bc29fca0c21d4f92562
Howon <hihowon310@naver.com>
2023-03-07 14:47:55 +0900

    Excel Data 읽어서 Debug Log 에 표시했다.

---
+++ Test_UnityToGit/Assets/01.Scripts/GetData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetData : MonoBehaviour
+{
+    public Entity_Sheet1 entity_Sheet1;
+
+    void Start()
+    {
+        foreach(Entity_Sheet1.Param param in entity_Sheet1.sheets[0].list)
+        {
+            Debug.Log(param.index + " - " + param.character + " - " + param.Hp + " - " + param.Hp);
+        }   
+    }
+}
